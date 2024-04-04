f5fce354a553211c468147fdfa72dec3a0e620ea
bote112 <cosmin-adrian.botezatu@s.unibuc.ro>
2024-04-04 16:44:35 +0300

    adaug

--- Proiect/src/Model/Camera.java
+++ Proiect/src/Model/Camera.java
@@ -1,4 +1,70 @@
 package Model;
 
 public class Camera {
+    private int id;
+    private int etaj;
+    private boolean ocupata;
+    private boolean curata;
+    private boolean balcon;
+
+    public Camera(int id, int etaj, boolean ocupata, boolean curata, boolean balcon) {
+        this.id = id;
+        this.etaj = etaj;
+        this.ocupata = ocupata;
+        this.curata = curata;
+        this.balcon = balcon;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getEtaj() {
+        return etaj;
+    }
+
+    public boolean isOcupata() {
+        return ocupata;
+    }
+
+    public boolean isCurata() {
+        return curata;
+    }
+
+    public boolean isBalcon() {
+        return balcon;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEtaj(int etaj) {
+        this.etaj = etaj;
+    }
+
+    public void setOcupata(boolean ocupata) {
+        this.ocupata = ocupata;
+    }
+
+    public void setCurata(boolean curata) {
+        this.curata = curata;
+    }
+
+    public void setBalcon(boolean balcon) {
+        this.balcon = balcon;
+    }
+
+    @Override
+    public String toString() {
+        return "Camera{" +
+                "id=" + id +
+                ", etaj=" + etaj +
+                ", ocupata=" + ocupata +
+                ", curata=" + curata +
+                ", balcon=" + balcon +
+                '}';
+    }
 }
--- Proiect/src/Model/Hotel.java
+++ Proiect/src/Model/Hotel.java
@@ -1,4 +1,39 @@
 package Model;
-
-public class Hotel {
-}
+/*
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ * public class Hotel {
+ * private String nume;
+ * private String adresa;
+ * private int nrEtaje;
+ * private <List>Camera camere;
+ * private <List>Rezervare rezervari;
+ * 
+ * public Hotel(String nume, String adresa, int nrEtaje, <List>Camera camere,
+ * <List>Rezervare rezervari) {
+ * this.nume = nume;
+ * this.adresa = adresa;
+ * this.nrEtaje = nrEtaje;
+ * this.camere = camere;
+ * this.rezervari = rezervari;
+ * }
+ * 
+ * // Getters
+ * public String getNume() { return nume; }
+ * public String getAdresa() { return adresa; }
+ * public int getNrEtaje() { return nrEtaje; }
+ * public <List>Camera getCamere() { return camere; }
+ * public <List>Rezervare getRezervari() { return rezervari; }
+ * 
+ * // Setters
+ * public void setNume(String nume) { this.nume = nume; }
+ * public void setAdresa(String adresa) { this.adresa = adresa; }
+ * public void setNrEtaje(int nrEtaje) { this.nrEtaje = nrEtaje; }
+ * public void setCamere(<List>Camera camere) { this.camere = camere; }
+ * public void setRezervari(<List>Rezervare rezervari) { this.rezervari =
+ * rezervari; }
+ * 
+ * 
+ * }
+ */
