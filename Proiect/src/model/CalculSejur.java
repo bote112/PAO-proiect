b23fe490dea64986f720ba7da2221da177c6182c
bote112 <cosmin-adrian.botezatu@s.unibuc.ro>
2024-04-06 21:06:00 +0300

    Cod cu erori + modificare CalculPretPerNoapte
    Am adaugat clasa de CalculPretPerNoapte pentru a reformata cum se calculeaza pretul - exista o eroare ce persista in cadrul clasei CalculSejur pentru ca se incarca folosirea gettterului pentru pretPerNoapte direct din clasa abstracta, lucru ce este imposibil

--- Proiect/src/model/CalculSejur.java
+++ Proiect/src/model/CalculSejur.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 
 public class CalculSejur {
     public static int calculeazaPretSejur(Rezervare rezervare) {
-        Camera camera = CameraFactory.createCamera(0, 0, false, false, false, 0);
+        Camera camera = CameraFactory.createCamera(rezervare.getCamera().getId(), rezervare.getCamera().getEtaj(),
+                rezervare.getCamera().isOcupata(), rezervare.getCamera().isCurata(), rezervare.getCamera().isBalcon(),
+                rezervare.getCamera().getPretPeNoapte());
         LocalDate dataInceput = rezervare.getDataInceput();
         LocalDate dataSfarsit = rezervare.getDataSfarsit();
     }
